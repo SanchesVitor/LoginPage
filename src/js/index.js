d9abce9ea7864519df6c6451487ef278ad136b08
SanchesVitor <vitorsanches06@hotmail.com>
2023-03-31 20:36:52 -0300

    add js and updated styles with media

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+function togglePassword() {
+  document.querySelectorAll('.eye').forEach((eye) => eye.classList.toggle('hide'));
+
+  const type = senha.getAttribute('type') == 'password' ? 'text' : 'password';
+  senha.setAttribute('type', type);
+}
